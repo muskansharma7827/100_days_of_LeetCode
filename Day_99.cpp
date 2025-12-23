afa5cc9162edca6903d185e306ab16720f76e037
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2025-12-23 19:01:34 +0530

    Add Day_99 solutions

---
+++ Day_99.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i + 1);
+            int max_len = max(odd, even);
+            if (max_len > end - start) {
+                start = i - (max_len - 1) / 2;
+                end   = i + max_len / 2;
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
+private:
+    int expandAroundCenter(string &s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "babad";
+    string result = sol.longestPalindrome(s);
+    cout << "Longest Palindromic Substring: " << result << endl;
+
+    s = "cbbd";
+    result = sol.longestPalindrome(s);
+    cout << "Longest Palindromic Substring: " << result << endl;
+
+    return 0;
+}
